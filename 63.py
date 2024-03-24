6c35bd27a77e2769bde1593876e736629b0fb055
basit <basitali23@gmail.com>
2024-03-24 17:34:42 +0500

    My initial commit for this file

---
+++ 63.py
@@ -0,0 +1,34 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<div class = "para" id = "para1">
+ABC
+</div>
+
+<div class = "para" id = "para2">
+
+</div>
+
+<div class = "para" id = "para3">
+</div>
+
+<div class = "para" id = "para4">
+</div>
+
+<div class = "para" id = "para5">
+</div>
+
+<div class = "para" id = "para6">
+</div>
